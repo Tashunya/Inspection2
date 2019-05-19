6e4f61b3a7094d0a98291f25ee5ee1f667fef36a
Tashunya <tashunya_17@mail.ru>
2019-05-19 16:17:30 +0300

    add account confirmation

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,8 +1,22 @@
 from flask import render_template, redirect, request, url_for, flash
-from flask_login import login_user, logout_user, login_required
+from flask_login import login_user, logout_user, login_required, current_user
 from . import auth
+from .. import db
 from ..models import User
 from .forms import LoginForm, RegistrationForm
+from ..email import send_email
+
+@auth.before_app_request
+def before_request():
+    if current_user.is_authenticated() and not current_user.confirmed and request.endpoint[:5] != 'auth.':
+        return redirect(url_for('auth.unconfirmed'))
+
+@auth.route("/unconfirmed")
+def unconfirmed():
+    if current_user.is_anonymous() or current_user.confirmed:
+        return redirect("main.index")
+    return render_template('auth/unconfirmed.html')
+
 
 @auth.route("/login", methods=["GET", "POST"])
 def login():
@@ -32,6 +46,21 @@ def register():
                     role = form.role.data,
                     company = form.company.data)
         db.session.add(user)
-        flash("User created.")
-        return redirect(url_for("auth.login"))
+        db.session.commit()
+        token = user.generate_confirmation_token()
+        send_email(user.email, "Finish Registration", 'auth/email/confirm', user=user, token=token)
+        flash("User created. A letter have been sent to finish registration.")
+        return redirect(url_for("main.index"))
     return render_template("auth/register.html", form=form)
+
+@auth.route("/confirm/<token>")
+@login_required
+def confirm(token):
+    if current_user.confirmed:
+        return redirect(url_for('main.index'))
+    if current_user.confirm(token):
+        flash("You have set password for your account. Thanks!")
+    else:
+        flash("The confirmation link is invalid or has expired.")
+    return redirect(url_for('main.index'))
+
--- app/models.py
+++ app/models.py
@@ -1,6 +1,8 @@
 from . import db
 from . import login_manager
 from werkzeug.security import generate_password_hash, check_password_hash
+from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
+from flask import current_app
 
 
 class Role(db.Model):
@@ -31,6 +33,7 @@ class User(db.Model):
     username = db.Column(db.String(64), index=True)
     password_hash = db.Column(db.String(128))
     contact_number = db.Column(db.String(64))
+    confirmed = db.Column(db.Boolean, default=False)
     role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
     company_id = db.Column(db.Integer, db.ForeignKey('companies.id'))
 
@@ -47,6 +50,22 @@ class User(db.Model):
 
     def verify_password(self, password):
         return check_password_hash(self.password_hash, password)
+
+    def generate_confirmation_token(self, expiration=3600):
+        s = Serializer(current_app.config['SECRET_KEY'], expiration)
+        return s.dumps({'confirm': self.id})
+
+    def confirm(self, token):
+        s = Serializer(current_app.config['SECRET_KEY'])
+        try:
+            data = s.loads(token)
+        except:
+            return False
+        if data.get('confirm') != self.id:
+            return False
+        self.confirmed = True
+        db.session.add(self)
+        return True
 
 @login_manager.user_loader
 def load_user(user_id):
