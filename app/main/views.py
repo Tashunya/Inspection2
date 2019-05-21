3684cb493844a57ab86fba8d689c50cea4d4f6dc
Tashunya <tashunya_17@mail.ru>
2019-05-21 10:26:54 +0300

    add role permissions

--- app/main/views.py
+++ app/main/views.py
@@ -1,19 +1,23 @@
-from flask import render_template, session, redirect, url_for, current_app, flash
+from flask import render_template, session, redirect, url_for, current_app, flash, abort
 from flask_login import login_required, current_user
 from . import main
 from .. import db
-from ..models import Company, Role
+from ..models import User, Company, Role
 
 
 @main.route('/')
 def index():
-    if current_user.is_anonymous:
-        return redirect(url_for('auth.login'))
     company = Company.query.filter_by(id=current_user.company_id).first()
     role = Role.query.filter_by(id=current_user.role_id).first()
-    return render_template('index.html', company=company, role = role)
+    if current_user.is_anonymous:
+        return redirect(url_for('auth.login'))
+    return render_template('index.html', company=company, role=role)
 
-@main.route('/user/<username>')
+@main.route('/user/<id>')
 @login_required
-def account():
-    return render_template('user.html')
+def user(id):
+    company = Company.query.filter_by(id=current_user.company_id).first()
+    role = Role.query.filter_by(id=current_user.role_id).first()
+
+    user = User.query.filter_by(id=id).first_or_404()
+    return render_template('user.html', user=user, company=company, role=role)
--- app/models.py
+++ app/models.py
@@ -1,24 +1,75 @@
 from werkzeug.security import generate_password_hash, check_password_hash
 from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
 from flask import current_app
-from flask_login import UserMixin
+from flask_login import UserMixin, AnonymousUserMixin
 from . import db, login_manager
+
+
+class Permission:
+    OWN_BOILER_ACCESS = 1
+    ALL_BOILERS_ACCESS = 2
+    BOILER_DATA_UPLOAD = 4
+    USER_REGISTER = 8
+    REPORT_DOWNLOAD = 16
+    EDIT_BOILER_DATA = 32
+    COMPANY_REGISTER = 64
+    CREATE_BOILER = 128
+    VIEW_PROFILES = 256
 
 
 class Role(db.Model):
     __tablename__ = 'roles'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(64), unique=True)
-    default = db.Column(db.Boolean, default=False, index=True)
     permissions = db.Column(db.Integer)
     users = db.relationship('User', backref='role', lazy='dynamic')
+
+    def __init__(self, **kwargs):
+        super(Role, self).__init__(**kwargs)
+        if self.permissions is None:
+            self.permissions = 0
+
+    @staticmethod
+    def insert_roles():
+        roles = {
+            'Repr': [Permission.OWN_BOILER_ACCESS, Permission.REPORT_DOWNLOAD],
+            'Inspector': [Permission.ALL_BOILERS_ACCESS, Permission.BOILER_DATA_UPLOAD,
+                          Permission.EDIT_BOILER_DATA, Permission.REPORT_DOWNLOAD,
+                          Permission.VIEW_PROFILES],
+            'Administrator': [Permission.ALL_BOILERS_ACCESS, Permission.BOILER_DATA_UPLOAD,
+                              Permission.USER_REGISTER, Permission.REPORT_DOWNLOAD,
+                              Permission.EDIT_BOILER_DATA, Permission.COMPANY_REGISTER,
+                              Permission.CREATE_BOILER, Permission.VIEW_PROFILES]
+        }
+
+        for r in roles:
+            role = Role.query.filter_by(name=r).first()
+            if role is None:
+                role = Role(name=r)
+            role.reset_permissions()
+            for perm in roles[r]:
+                role.add_permission(perm)
+            db.session.add(role)
+        db.session.commit()
+
+    def add_permission(self, perm):
+        if not self.has_permission(perm):
+            self.permissions += perm
+
+    def remove_permission(self, perm):
+        if self.has_permission(perm):
+            self.permissions -= perm
+
+    def reset_permissions(self):
+        self.permissions = 0
+
+    def has_permission(self, perm):
+        return self.permissions & perm == perm
 
     def __repr__(self):
         return '<Role %r>' % self.name
 
-class Permission:
-    pass
-
+#==========================================
 
 class User(UserMixin, db.Model):
     __tablename__ = 'users'
@@ -32,8 +83,6 @@ class User(UserMixin, db.Model):
     role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
     company_id = db.Column(db.Integer, db.ForeignKey('companies.id'))
 
-    def __repr__(self):
-        return '<User %r>' % self.username
 
     @property
     def password(self):
@@ -62,10 +111,28 @@ class User(UserMixin, db.Model):
         db.session.add(self)
         return True
 
+    def can(self, perm):
+        return self.role is not None and self.role.has_permission(perm)
+
+    # def is_administrator(self):
+    #     return self.can(Permission.ADMIN)
+
+    def __repr__(self):
+        return '<User %r>' % self.username
+
+
+class AnonymousUser(AnonymousUserMixin):
+    def can(self, permissions):
+        return False
+
+login_manager.anonymous_user = AnonymousUser
+
 @login_manager.user_loader
 def load_user(user_id):
     return User.query.get(int(user_id))
 
+
+# ============================================
 
 class Company(db.Model):
     __tablename__ = 'companies'
